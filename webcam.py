d8ad9fe17a0067720d76a6803b22c8db84be9e0e
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-02-02 12:47:19 +0800

    chore: remove extraneous arg parsing function

--- webcam.py
+++ webcam.py
@@ -1,35 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-import logging
-from argparse import ArgumentParser
-
-from mmcv import Config, DictAction
+from mmcv import Config
 
 from mmpose.apis.webcam import WebcamExecutor
 from mmpose.apis.webcam.nodes import model_nodes
-
-
-def parse_args():
-    parser = ArgumentParser('Webcam executor configs')
-    parser.add_argument(
-        '--config', type=str, default='demo/webcam_cfg/pose_tracking.py')
-
-    parser.add_argument(
-        '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        default={},
-        help='Override settings in the config. The key-value pair '
-        'in xxx=yyy format will be merged into config file. For example, '
-        "'--cfg-options executor_cfg.camera_id=1'")
-    parser.add_argument(
-        '--debug', action='store_true', help='Show debug information.')
-    parser.add_argument(
-        '--cpu', action='store_true', help='Use CPU for model inference.')
-    parser.add_argument(
-        '--cuda', action='store_true', help='Use GPU for model inference.')
-
-    return parser.parse_args()
 
 
 def set_device(cfg: Config, device: str):
